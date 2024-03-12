e695fd7d395b6791c3fc0db098c28496a1a207bc
moushumi07 <104206237+moushumi07@users.noreply.github.com>
2024-03-12 09:30:02 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Docker Container Executed")
+a=input('enter your name: ')
+print('Welcome',a,'to world of Dock')
+
+
+# In[ ]:
+
+
+
+
